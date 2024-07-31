32c27f7985444a2b21ccc6aa0030ad1b3155c9d3
Alex Semionov <alexandralexv@gmail.com>
2024-07-31 18:16:56 +0300

    5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const carouselEl = document.querySelector(".carousel__inner");
+  const buttonRightEl = document.querySelector(".carousel__arrow_right");
+  const buttonLeftEl = document.querySelector(".carousel__arrow_left");
+
+  buttonLeftEl.style.display = "none";
+
+  let widthOfEl = 0;
+
+  buttonRightEl.addEventListener("click", function () {
+    widthOfEl += -carouselEl.offsetWidth;
+    carouselEl.style.transform = `translateX(${widthOfEl}px)`;
+    console.log(widthOfEl);
+    if (widthOfEl == -(carouselEl.offsetWidth * 3)) {
+      buttonRightEl.style.display = "none";
+    } else {
+      buttonRightEl.style.display = "";
+      buttonLeftEl.style.display = "";
+    }
+  });
+
+  buttonLeftEl.addEventListener("click", function () {
+    widthOfEl += carouselEl.offsetWidth;
+    carouselEl.style.transform = `translateX(${widthOfEl}px)`;
+    console.log(widthOfEl);
+    if (widthOfEl == 0) {
+      buttonLeftEl.style.display = "none";
+    } else {
+      buttonLeftEl.style.display = "";
+      buttonRightEl.style.display = "";
+    }
+  });
 }
