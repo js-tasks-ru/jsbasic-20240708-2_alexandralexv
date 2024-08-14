fb88bb1666c20db87f5ef5c3b585efd2e39055f7
Alex Semionov <alexandralexv@gmail.com>
2024-08-14 22:00:11 +0300

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,42 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement("table");
+
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    `;
+
+    this.tbody = this.elem.querySelector("tbody");
+
+    rows.forEach((row) => {
+      this.addRow(row);
+    });
+  }
+
+  addRow(row) {
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${row.name}</td>
+      <td>${row.age}</td>
+      <td>${row.salary}</td>
+      <td>${row.city}</td>
+      <td><button>X</button></td>
+    `;
+
+    tr.querySelector("button").addEventListener("click", () => {
+      tr.remove();
+    });
+
+    this.tbody.append(tr);
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,112 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.elem = this.createInner();
+  }
+
+  createInner() {
+    const carousel = createElement(`
+      <div class="carousel">
+
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+
+        <div class="carousel__inner">
+        </div>
+      </div>
+    `);
+
+    this.createCard(carousel);
+    this.carouselShift(carousel);
+    this.addToBascet(carousel);
+    return carousel;
+  }
+
+  createCard(carousel) {
+    const innerEl = carousel.querySelector(".carousel__inner");
+
+    for (let i = 0; i < this.slides.length; i++) {
+      let slide = this.slides[i];
+
+      const card = createElement(`
+        <div class="carousel__slide" data-id="${slide.id}">
+          <img src="/assets/images/carousel/${
+            slide.image
+          }" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+            <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+        </div>
+      `);
+
+      innerEl.appendChild(card);
+    }
+  }
+  addToBascet(carousel) {
+    console.log("addToBascet()");
+    const carouselList = carousel.querySelector(".carousel__inner");
+
+    carouselList.addEventListener("product-add", (event) => {
+      console.log("Блюдо добавлено в корзину:", event.detail);
+    });
+
+    carouselList.addEventListener("click", ({ target }) => {
+      let btn = target.closest("button");
+
+      if (btn) {
+        let product = btn.closest("div[data-id]").getAttribute("data-id");
+
+        const ce = new CustomEvent("product-add", {
+          detail: product,
+          bubbles: true,
+        });
+
+        carouselList.dispatchEvent(ce);
+      }
+    });
+  }
+
+  carouselShift(carousel) {
+    let countSlide = this.slides.length - 1;
+
+    const carouselEl = carousel.querySelector(".carousel__inner");
+    const buttonRightEl = carousel.querySelector(".carousel__arrow_right");
+    const buttonLeftEl = carousel.querySelector(".carousel__arrow_left");
+
+    buttonLeftEl.style.display = "none";
+
+    let widthOfEl = 0;
+
+    buttonRightEl.addEventListener("click", function () {
+      widthOfEl += -carouselEl.offsetWidth;
+      carouselEl.style.transform = `translateX(${widthOfEl}px)`;
+      if (widthOfEl == -(carouselEl.offsetWidth * countSlide)) {
+        buttonRightEl.style.display = "none";
+      } else {
+        buttonRightEl.style.display = "";
+        buttonLeftEl.style.display = "";
+      }
+    });
+
+    buttonLeftEl.addEventListener("click", function () {
+      widthOfEl += carouselEl.offsetWidth;
+      carouselEl.style.transform = `translateX(${widthOfEl}px)`;
+      if (widthOfEl == 0) {
+        buttonLeftEl.style.display = "none";
+      } else {
+        buttonLeftEl.style.display = "";
+        buttonRightEl.style.display = "";
+      }
+    });
   }
 }
